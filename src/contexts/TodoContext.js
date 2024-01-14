93aa81a3ba6d5b0ca0abf5b9e26388a427a68cc3
kanwar Dhillon <kdhillon469@gmail.com>
2024-01-15 04:19:03 +1100

    added todo context

---
+++ src/contexts/TodoContext.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos: [],
+    addTodo: (todo) => { },
+    updateTodo: (id, todo) => { },
+    deleteTodo: (id) => { },
+    toggleComplete: (id) => { }
+
+})
+
+export const useTodo = () => {
+    return useContext(TodoContext)
+
+}
+export const TodoProvider = TodoContext.Provider;
